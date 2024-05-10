ab559fcebdf30197c9f8bff024373ec39ae22dce
rinalditomas <rinalditomas@gmail.com>
2024-05-10 13:24:16 +0200

    Added loading message

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,27 +1,24 @@
 import Movie from "./Movie";
 import "../styles/movies.scss";
 
-const Movies = ({
-  movies,
-  viewTrailer,
-}) => {
-
+const Movies = ({ movies, viewTrailer }) => {
   return (
-    <div data-testid="movies" className="movies-grid">
-      {movies.movies.results?.map((movie) => {
-        return (
-          <Movie
-            movie={movie}
-            key={movie.name}
-            viewTrailer={viewTrailer}
-          />
-        );
-      })}
+    <div>
+      <div data-testid="movies" className="movies-grid">
+        {movies.movies.results?.map((movie) => {
+          return (
+            <Movie movie={movie} key={movie.name} viewTrailer={viewTrailer} />
+          );
+        })}
+      </div>
+      {movies.fetchStatus === "loading" && (
+        <div className="loading-container">
+          <h2 className="loading">Loading...</h2>
+        </div>
+      )}
       <div id="observe-end"></div>
     </div>
   );
 };
 
 export default Movies;
-
-

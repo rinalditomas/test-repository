305d16841aec6ded2e198a3fdcfbfb7a3f53dea9
rinalditomas <rinalditomas@gmail.com>
2024-05-10 09:00:19 +0200

    Updated tests

--- src/test/movieSlice.test.js
+++ src/test/movieSlice.test.js
@@ -1,36 +1,42 @@
-import moviesSlice, { fetchMovies } from '../data/moviesSlice'
-import { moviesMock } from './movies.mocks'
+import moviesSlice, { fetchMovies } from "../data/moviesSlice";
+import { moviesMock } from "./movies.mocks";
 
-describe('MovieSlice test', () => {
-    
-    it('should set loading true while action is pending', () => {
-        const action = {type: fetchMovies.pending};
-        const initialState = moviesSlice.reducer(
-        { 
-            movies: [], fetchStatus: '',
-        }, action);
-        expect(action).toEqual({type: fetchMovies.pending})
-     })
+describe("MovieSlice test", () => {
+  it("should set loading true while action is pending", () => {
+    const action = { type: fetchMovies.pending };
+    const initialState = moviesSlice.reducer(
+      {
+        movies: [],
+        fetchStatus: "",
+      },
+      action
+    );
+    expect(action).toEqual({ type: fetchMovies.pending });
+  });
 
-    it('should return payload when action is fulfilled', () => {
-        const action = {
-            type: fetchMovies.fulfilled, 
-            payload: moviesMock
-        };
-        const initialState = moviesSlice.reducer(
-        { 
-            movies: [], fetchStatus: '',
-        }, action);
-        expect(action.payload).toBeTruthy()
-    })
-
-    it('should set error when action is rejected', () => {
-        const action = {type: fetchMovies.rejected};
-        const initialState = moviesSlice.reducer(
-        { 
-            movies: [], fetchStatus: '',
-        }, action);
-        expect(action).toEqual({type: fetchMovies.rejected})
-     })
-
-})
+  it("should return payload when action is fulfilled", () => {
+    const action = {
+      type: fetchMovies.fulfilled,
+      payload: moviesMock,
+    };
+    const initialState = moviesSlice.reducer(
+      {
+        movies: { results: [] },
+        fetchStatus: "", // Add results: [] here
+      },
+      action
+    );
+    expect(action.payload).toBeTruthy();
+  });
+  it("should set error when action is rejected", () => {
+    const action = { type: fetchMovies.rejected };
+    const initialState = moviesSlice.reducer(
+      {
+        movies: [],
+        fetchStatus: "",
+      },
+      action
+    );
+    expect(action).toEqual({ type: fetchMovies.rejected });
+  });
+});
--- src/test/watchLaterSlice.test.js
+++ src/test/watchLaterSlice.test.js
@@ -1,35 +1,38 @@
-import watchLaterSlice from '../data/watchLaterSlice'
-import { moviesMock } from './movies.mocks'
+import watchLaterSlice from "../data/watchLaterSlice";
+import { starredMoviesMock } from "./starredMovies.mocks";
 
-describe('watchLaterSlice test', () => {
+describe("watchLaterSlice test", () => {
+  const state = { watchLaterMovies: [] };
 
-    const state = { watchLaterMovies: [] }
+  it("should set initial state", () => {
+    const initialState = state;
+    const action = { type: "" };
+    const result = watchLaterSlice.reducer(initialState, action);
+    expect(result).toEqual({ watchLaterMovies: [] });
+  });
 
-    it('should set initial state', () => {
-        const initialState = state
-        const action = { type: '' }
-        const result = watchLaterSlice.reducer(initialState, action)
-        expect(result).toEqual({ watchLaterMovies: []})
-      })    
+  it("should add movie to watch later", () => {
+    const initialState = { ...state, watchLaterMovies: [] };
+    const action = watchLaterSlice.actions.addToWatchLater(
+      starredMoviesMock[0]
+    );
+    const result = watchLaterSlice.reducer(initialState, action);
+    expect(result.watchLaterMovies[0]).toBe(starredMoviesMock[0]);
+  });
 
-      it('should add movie to watch later', () => {
-        const initialState = { ...state, watchLaterMovies: [] }
-        const action = watchLaterSlice.actions.addToWatchLater(moviesMock[0])
-        const result = watchLaterSlice.reducer(initialState, action)
-        expect(result.watchLaterMovies[0]).toBe(moviesMock[0])
-      })
+  it("should remove movie from watch later", () => {
+    const initialState = { ...state, watchLaterMovies: starredMoviesMock };
+    const action = watchLaterSlice.actions.removeFromWatchLater(
+      starredMoviesMock[0]
+    );
+    const result = watchLaterSlice.reducer(initialState, action);
+    expect(result.watchLaterMovies[0]).toBe(starredMoviesMock[1]);
+  });
 
-      it('should remove movie from watch later', () => {
-        const initialState = { ...state, watchLaterMovies: moviesMock }
-        const action = watchLaterSlice.actions.removeFromWatchLater(moviesMock[0])
-        const result = watchLaterSlice.reducer(initialState, action)
-        expect(result.watchLaterMovies[0]).toBe(moviesMock[1])
-      })
-
-      it('should remove all movies', () => {
-        const initialState = { ...state, watchLaterMovies: moviesMock }
-        const action = watchLaterSlice.actions.remveAllWatchLater(state)
-        const result = watchLaterSlice.reducer(initialState, action)
-        expect(Object.keys(result.watchLaterMovies).length).toEqual(0)
-      })
-})
+  it("should remove all movies", () => {
+    const initialState = { ...state, watchLaterMovies: starredMoviesMock };
+    const action = watchLaterSlice.actions.removeAllWatchLater(state);
+    const result = watchLaterSlice.reducer(initialState, action);
+    expect(Object.keys(result.watchLaterMovies).length).toEqual(0);
+  });
+});

661fb4bf07cf6a1191a1732be5d91196746791ea
rinalditomas <rinalditomas@gmail.com>
2024-05-10 08:33:56 +0200

    replaced hard coded api key and base endpoints for env variables

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_KEY = '8cac6dec66e09ab439c081b251304443'
-export const ENDPOINT = 'https://api.themoviedb.org/3'
+export const API_KEY = process.env.REACT_APP_API_KEY;
+export const ENDPOINT = process.env.REACT_APP_API_URL;
 export const ENDPOINT_DISCOVER = ENDPOINT+'/discover/movie?api_key='+API_KEY+'&sort_by=vote_count.desc'
 export const ENDPOINT_SEARCH = ENDPOINT+'/search/movie?api_key='+API_KEY

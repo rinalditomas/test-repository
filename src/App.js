e5633e730179608cbd092c9bec2e302fccd202c2
rinalditomas <rinalditomas@gmail.com>
2024-05-10 08:29:08 +0200

    Add Modal component for displaying the movie trailer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
-import { useEffect, useState } from 'react'
-import { Routes, Route, createSearchParams, useSearchParams, useNavigate } from "react-router-dom"
-import { useDispatch, useSelector } from 'react-redux'
-import 'reactjs-popup/dist/index.css'
-import { fetchMovies } from './data/moviesSlice'
-import { ENDPOINT_SEARCH, ENDPOINT_DISCOVER, ENDPOINT, API_KEY } from './constants'
-import Header from './components/Header'
-import Movies from './components/Movies'
-import Starred from './components/Starred'
-import WatchLater from './components/WatchLater'
-import YouTubePlayer from './components/YoutubePlayer'
-import './app.scss'
+import Modal from "./components/Modal";
 
 const App = () => {
 
@@ -79,13 +68,9 @@ const App = () => {
       <Header searchMovies={searchMovies} searchParams={searchParams} setSearchParams={setSearchParams} />
 
       <div className="container">
-        {videoKey ? (
-          <YouTubePlayer
-            videoKey={videoKey}
-          />
-        ) : (
-          <div style={{padding: "30px"}}><h6>no trailer available. Try another movie</h6></div>
-        )}
+        <Modal isOpen={isOpen} closeModal={closeModal}>
+          <YouTubePlayer videoKey={videoKey} />
+        </Modal>
 
         <Routes>
           <Route path="/" element={<Movies movies={movies} viewTrailer={viewTrailer} closeCard={closeCard} />} />

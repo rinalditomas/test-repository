ed98dec3c0975984fe8f7a01b381f2ceba7dc476
rinalditomas <rinalditomas@gmail.com>
2024-05-10 08:39:26 +0200

    refactored movieSlice to implement infinite scrolling

--- src/data/moviesSlice.js
+++ src/data/moviesSlice.js
@@ -1,27 +1,58 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchMovies = createAsyncThunk('fetch-movies', async (apiUrl) => {
-    const response = await fetch(apiUrl)
-    return response.json()
-})
+export const fetchMovies = createAsyncThunk("fetch-movies", async (apiUrl) => {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data;
+});
 
 const moviesSlice = createSlice({
-    name: 'movies',
-    initialState: { 
-        movies: [],
-        fetchStatus: '',
+  name: "movies",
+  initialState: {
+    movies: {
+      page: 1,
+      results: [],
+      total_pages: 0,
+      total_results: 0,
     },
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(fetchMovies.fulfilled, (state, action) => {
-            state.movies = action.payload
-            state.fetchStatus = 'success'
-        }).addCase(fetchMovies.pending, (state) => {
-            state.fetchStatus = 'loading'
-        }).addCase(fetchMovies.rejected, (state) => {
-            state.fetchStatus = 'error'
-        })
-    }
-})
+    fetchStatus: "",
+  },
+  reducers: {
+    clearState: (state) => {
+      state.movies = {
+        page: 1,
+        results: [],
+        total_pages: 0,
+        total_results: 0,
+        
+      };
+      state.fetchStatus = "";
+    },
+    incrementPage: (state, action) => {
+      if (state.movies.page < state.movies.total_pages) {
+        state.movies.page = state.movies.page + 1;
+      }
+    },
+    resetPage: (state) => {
+      state.movies.page = 1;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMovies.fulfilled, (state, action) => {
+        const newResults = [...state.movies.results, ...action.payload.results];
+        state.movies.results = [...new Set(newResults)];
+        state.movies.total_pages = action.payload.total_pages;
+        state.movies.total_results = action.payload.total_results;
+        state.fetchStatus = "success";
+      })
+      .addCase(fetchMovies.pending, (state) => {
+        state.fetchStatus = "loading";
+      })
+      .addCase(fetchMovies.rejected, (state) => {
+        state.fetchStatus = "error";
+      });
+  },
+});
 
-export default moviesSlice
+export default moviesSlice;

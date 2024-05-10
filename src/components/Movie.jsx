4f6ff7c3450fa5b0f50b304a31ab578e950b7dec
rinalditomas <rinalditomas@gmail.com>
2024-05-10 08:49:29 +0200

    remove unused code

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,7 +3,7 @@ import starredSlice from '../data/starredSlice'
 import watchLaterSlice from '../data/watchLaterSlice'
 import placeholder from '../assets/not-found-500X750.jpeg'
 
-const Movie = ({ movie, viewTrailer, closeCard }) => {
+const Movie = ({ movie, viewTrailer }) => {
   const starred = useSelector((state) => state.starred);
   const watchLater = useSelector((state) => state.watchLater);
   const { starMovie, unstarMovie } = starredSlice.actions;

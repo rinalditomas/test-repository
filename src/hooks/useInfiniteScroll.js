7f02769b01fdf5ef2dfbba1524594ab21727c9e8
rinalditomas <rinalditomas@gmail.com>
2024-05-12 18:09:24 +0200

    Refactor useInfiniteScroll hook to improve readability and add comments

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -10,25 +10,28 @@ export const useInfiniteScroll = (
 
   useEffect(() => {
     const options = {
-      root: null,
-      rootMargin: "20px",
-      threshold: 1.0,
+      root: null, // The element that the target element is compared to
+      rootMargin: "20px", // The margin around the root element
+      threshold: 1.0, // The percentage of the target element visible for the callback to be invoked
     };
 
+    // Create a new IntersectionObserver and store it in the ref
+    // the callback function is going to be called when the target element becomes visible
     observer.current = new IntersectionObserver(
       (entities) => callback(entities, dispatch),
       options
     );
 
+    // Disconnect the IntersectionObserver when the component is unmounted
     return () => {
       observer.current.disconnect();
     };
   }, []);
 
   useEffect(() => {
-    const element = document.getElementById(elementId);
+    const element = document.getElementById(elementId); // Get the element to observe
     if (element) {
-      observer.current.observe(element);
+      observer.current.observe(element); // Start observing the element
     }
   }, dependencies);
 };

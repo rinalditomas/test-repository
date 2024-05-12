21f8fbe5423fd2f16a1f34eaefe37d7ecb44e614
Tomas Rinaldi Scatena <76567220+rinalditomas@users.noreply.github.com>
2024-05-12 16:40:25 +0200

    Fix endpoints

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const API_KEY = process.env.REACT_APP_API_KEY;
 export const ENDPOINT = process.env.REACT_APP_API_URL;
-export const ENDPOINT_DISCOVER = ENDPOINT+'/discover/movie/?api_key='+API_KEY+'&sort_by=vote_count.desc'
-export const ENDPOINT_SEARCH = ENDPOINT+'/search/movie/?api_key='+API_KEY
+export const ENDPOINT_DISCOVER = ENDPOINT+'/discover/movie?api_key='+API_KEY+'&sort_by=vote_count.desc'
+export const ENDPOINT_SEARCH = ENDPOINT+'/search/movie?api_key='+API_KEY

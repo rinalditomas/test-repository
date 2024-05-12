8d5e77d3be45fcf3d12f9b02ee3ee4171a23eb25
Tomas Rinaldi Scatena <76567220+rinalditomas@users.noreply.github.com>
2024-05-12 16:39:45 +0200

    remove unused endpoitn

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,3 @@ export const API_KEY = process.env.REACT_APP_API_KEY;
 export const ENDPOINT = process.env.REACT_APP_API_URL;
 export const ENDPOINT_DISCOVER = ENDPOINT+'/discover/movie/?api_key='+API_KEY+'&sort_by=vote_count.desc'
 export const ENDPOINT_SEARCH = ENDPOINT+'/search/movie/?api_key='+API_KEY
-export const ENDPOINT_MOVIE = ENDPOINT+'/movie/507086?api_key='+API_KEY+'&append_to_response=videos'
